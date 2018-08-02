1629ef40b34a11c28672d81f51715adf118f3912
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-08-02 01:56:48 -0700

    Update and rename oddrange.java to oddrange2.java

--- oddrange2.java
+++ oddrange2.java
@@ -1,6 +1,10 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -13,7 +17,7 @@ class Ideone
 	         {
 	         	 if (i % 2 != 0)
 	         	 {
-	         	 	System.out.print(i + " ");
+	         	 	System.out.print(i);
 	         	 }
 	         }
 	}
